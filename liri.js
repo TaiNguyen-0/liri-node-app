a5f7df9466899cd046e22edcce4797f5a46f1c66
Tai <taing89@gmail.com>
2019-07-14 15:02:16 -0700

    Added date converter to bands in town date.

--- liri.js
+++ liri.js
@@ -26,31 +26,39 @@ axios.get("http://www.omdbapi.com/?t=" + "Brave" + "&y=&plot=short&apikey=trilog
     function (response) {
         // Then we print out the imdbRating
         // console.log(JSON.stringify(response.data, null, 2));
-        console.log("Title: " + response.data.Title);
-        console.log("Year: " + response.data.Year);
-        console.log("Rated: " + response.data.Rated);
-        console.log(response.data.Ratings[1].Source + " Rating: " + response.data.Ratings[1].Value);
-        console.log("Country: " + response.data.Country);
-        console.log("Language: " + response.data.Language);
-        console.log("Plot: " + response.data.Plot);
-        console.log("Actors: " + response.data.Actors);
+
+        // console.log("Title: " + response.data.Title);
+        // console.log("Year: " + response.data.Year);
+        // console.log("Rated: " + response.data.Rated);
+        // console.log(response.data.Ratings[1].Source + " Rating: " + response.data.Ratings[1].Value);
+        // console.log("Country: " + response.data.Country);
+        // console.log("Language: " + response.data.Language);
+        // console.log("Plot: " + response.data.Plot);
+        // console.log("Actors: " + response.data.Actors);
 
         //  If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
 
-    //  * If you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>
+        //  * If you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>
 
-    //  * It's on Netflix!
+        //  * It's on Netflix!
     }
 );
 
 // !!! change Jinjer back to artist
-axios.get("https://rest.bandsintown.com/artists/" + "Jinjer" + "/events?app_id=codingbootcamp").then(
+axios.get("https://rest.bandsintown.com/artists/" + "BabyMetal" + "/events?app_id=codingbootcamp").then(
     function (response) {
         // Then we print out the imdbRating
-        console.log(JSON.stringify(response.data.venue[1], null, 2));
-        // console.log("The band is playing at: " + response.data.venue.name);
-        // console.log("Location: " + response.data.venue.city + ", " + response.data.venue.region) + ", " + response.data.venue.country;
-        // console.log("Date MMDDYYYY: " + response.data.venue.datetime);
+        // console.log(JSON.stringify(response.data[0], null, 2));
+        console.log("Performing Artists: " + response.data[0].lineup);
+        console.log("Venue: " + response.data[0].venue.name);
+        console.log("City: " + response.data[0].venue.city);
+        console.log("State: " + response.data[0].venue.region);
+        console.log("Country: " + response.data[0].venue.country);
+
+        // date format convertor
+        var date = new Date(response.data[0].datetime);
+        var newdate = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
+        console.log("Date: " + newdate);
     }
 );
 
